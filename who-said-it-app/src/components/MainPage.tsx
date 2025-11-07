0b230b55f4ed2ea7657478eefdc93bfb49c0a786
Samantha Phillips <samanthaphillips886@gmail.com>
2025-11-06 20:30:48 -0600

    mini commit

--- who-said-it-app/src/components/MainPage.tsx
+++ who-said-it-app/src/components/MainPage.tsx
@@ -7,29 +7,56 @@ import { useState } from "react";
 
 const MainPage = () => {
   const navigate = useNavigate();
-  const randomIndex = Math.floor(Math.random() * quotes.length);
   const [playGame, setPlayGame] = useState(false);
   const [chosenQuote, setChosenQuote] = useState("");
+  const [chosenSpeaker, setChosenSpeaker] = useState("");
+  const [guess, setGuess] = useState("");
+  const [isCorrect, setIsCorrect] = useState(false);
 
   const handleClick = () => {
     navigate("/");
   };
 
   const playGameClick = () => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
     setPlayGame(true);
     setChosenQuote(quotes[randomIndex].quote);
+    setChosenSpeaker(quotes[randomIndex].speaker);
     console.log(`should be playing game`);
   };
+
+  const handleGuess = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    setIsCorrect(true);
+    console.log(`guess is: ${guess}`);
+  }
 
   return (
     <>
       <div>
         {playGame && (
-          <Typography variant="h1" gutterBottom>
-            {chosenQuote}
-          </Typography>
+          <>
+            <Typography variant="h1" gutterBottom>
+              {chosenQuote}
+            </Typography>
+            <form onSubmit={handleGuess}>
+              <input
+                type="text"
+                value={guess}
+                onChange={(e) => setGuess(e.target.value)}
+              />
+              <button type="submit">Enter</button>
+            </form>
+
+          </>
         )}
       </div>
+      <>
+        {isCorrect ? (
+          <h1>CORRECT</h1>
+        ) : <h1>FALSE</h1>}
+
+      </>
 
       <ThemeProvider theme={theme}>
         <Stack direction="row" spacing={2}>
